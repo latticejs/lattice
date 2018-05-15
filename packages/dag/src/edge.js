f90840519ccb5e0ee33fac20b45181e6b13cc6af
Diego Paez <paez.di@gmail.com>
2018-05-15 16:12:58 -0300

    Update: using theme + marker arrow

--- packages/dag/src/edge.js
+++ packages/dag/src/edge.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import { select } from 'd3-selection';
 import { DEFAULTS } from './dag';
 
 const enterEdge = (selection, theme) => {
   selection
-    .attr('stroke-width', 2)
-    .attr('fill', 'purple') // use theme
-    .attr('stroke', 'black')  // use theme
+    .attr("stroke-width", 1)
+    .attr('marker-end', 'url(#end)');
 }
 
 const updateEdge = (selection) => {
@@ -33,7 +33,8 @@ export default class Edge extends Component {
   render() {
     return (
       <line ref={node => this.node = node}
-        className={DEFAULTS.linkClass} />
+        className={classNames(DEFAULTS.linkClass, this.props.classes.dagEdge)} />
     );
   }
 }
+
