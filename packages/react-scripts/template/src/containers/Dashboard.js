e4b4b045a1748aeb19242ea139358aa851bffef9
Diego Paez <paez.di@gmail.com>
2018-05-21 11:25:31 -0300

    Update: add task scheduler inside dashboard container

--- packages/react-scripts/template/src/containers/Dashboard.js
+++ packages/react-scripts/template/src/containers/Dashboard.js
@@ -11,6 +11,7 @@ import AverageRevenue from '../components/dashboard/AverageRevenue';
 import NewCustomers from '../components/dashboard/NewCustomers';
 import Sales from '../components/dashboard/Sales';
 import Stats from '../components/dashboard/Stats';
+import TaskScheduler from '../components/dashboard/TaskScheduler';
 import allSales from '../queries/sales';
 import allStats from '../queries/stats';
 
@@ -40,7 +41,14 @@ class Dashboard extends Component {
             </Grid>
             <Grid item xs={12} md={4}>
               <AverageRevenue />
-            </Grid>    
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container alignItems="stretch" spacing={16}>
+            <Grid item xs={12} md={8}>
+              <TaskScheduler />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
