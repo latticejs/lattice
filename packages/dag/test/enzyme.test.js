9cadaff61051e2a207eee33057ef68775d3e8465
Diego <paez.di@gmail.com>
2018-06-01 16:03:22 -0300

    Enzyme dag (#46)
    * Update: wip enzyme integration
    
    * Update: remove unused jest-enzyme, customize jest script file
    
    * Update: add simple enzyme test
    
    * Add: enzyme setup config
    
    * Update: remove console.log
    
    * Update: remove unnecessary  peerdeps

---
+++ packages/dag/test/enzyme.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Dag from '../';
+
+describe('<MyComponent />', () => {
+	it('renders one <Dag /> component', () => {
+    const wrapper = mount(<Dag 
+      width={500}
+      height={500}
+      nodes={[]} 
+      edges={[]}/>);
+		expect(wrapper.find('.dag-wrapper').length).toBe(1);
+	});
+})
