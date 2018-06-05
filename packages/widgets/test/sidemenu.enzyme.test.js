fef67654783ea1e7723fef220816d439e1cd66f0
Diego Paez <paez.di@gmail.com>
2018-06-05 16:00:26 -0300

    Add: reorganize tests into different files

---
+++ packages/widgets/test/sidemenu.enzyme.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { SideMenu } from '../';
+
+const sideMenuMockClick = jest.fn();
+const navigation = [
+  {
+    path: '/employees',
+    title: 'Employees',
+    component: () => (<div>dummy</div>)
+  }
+]
+
+describe('<SideMenu />', () => {
+  it('has a functioning click handler', () => {
+    const wrapper = mount(
+      <SideMenu
+        width={200}
+        navigation={navigation}
+        onItemClick={sideMenuMockClick}
+      />
+    );
+		expect(wrapper.find(SideMenu).length).toBe(1);
+    // simulate click on item, finding children using displayname
+    wrapper.find('ButtonBase').simulate('click');
+    expect(sideMenuMockClick.mock.calls.length).toEqual(1);
+	});
+})
+
