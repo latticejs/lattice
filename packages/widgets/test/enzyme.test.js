e823db018e1600e18dd7d5616284f4df6144c501
Diego Paez <paez.di@gmail.com>
2018-06-05 15:15:13 -0300

    Update: add sidemenu enzyme test

--- packages/widgets/test/enzyme.test.js
+++ packages/widgets/test/enzyme.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import { Widget, Sidement } from '../';
+import { Widget, SideMenu } from '../';
 
 describe('<Widget />', () => {
-	it('renders one empty <Widget /> component', () => {
+	it('renders one empty <Widget /> component with a title', () => {
     const wrapper = mount(
       <Widget 
         title={'test widget'}
@@ -12,5 +12,31 @@ describe('<Widget />', () => {
       </Widget>
     );
 		expect(wrapper.find('h2').length).toBe(1);
+		expect(wrapper.find('h2').text()).toBe('test widget');
 	});
 })
+
+const sideMenuMockClick = jest.fn();
+const navigation = [
+  {
+    path: '/employees',
+    title: 'Employees',
+    component: () => (<div>dummy</div>)
+  }
+]
+describe('<SideMenu />', () => {
+  it('has a functioning click handler', () => {
+    const wrapper = mount(
+      <SideMenu
+        width={200}
+        navigation={navigation}
+        onItemClick={sideMenuMockClick}
+      />
+    );
+		expect(wrapper.find(SideMenu).length).toBe(1);
+    // simulate click on item, finding children using displayname
+    wrapper.find('ButtonBase').simulate('click');
+    expect(sideMenuMockClick.mock.calls.length).toEqual(1);
+	});
+})
+
