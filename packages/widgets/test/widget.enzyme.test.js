4440fabc101a578a7097836491bb79a89c388692
Diego <paez.di@gmail.com>
2018-06-06 10:51:02 -0300

    Dk/enzyme widgets (#56)
    * Update: add enzyme deps along with jest setup
    
    * Add: enzyme wrapper
    
    * Add: initial enzyme test for widget component
    
    * Update: deps
    
    * Update: add sidemenu enzyme test
    
    * Remove: split into 2 new files
    
    * Add: reorganize tests into different files

---
+++ packages/widgets/test/widget.enzyme.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { Widget } from '../';
+
+describe('<Widget />', () => {
+	it('renders one empty <Widget /> component with a title', () => {
+    const wrapper = mount(
+      <Widget
+        title={'test widget'}
+      >
+      </Widget>
+    );
+		expect(wrapper.find('h2').length).toBe(1);
+		expect(wrapper.find('h2').text()).toBe('test widget');
+	});
+})
