ecf6a35ca334466ac0c36623dbf101fc65166827
Diego Paez <paez.di@gmail.com>
2018-06-06 15:06:17 -0300

    Update: lint fix

--- packages/widgets/test/widget.enzyme.test.js
+++ packages/widgets/test/widget.enzyme.test.js
@@ -4,14 +4,9 @@ import { mount } from 'enzyme';
 import { Widget } from '../';
 
 describe('<Widget />', () => {
-	it('renders one empty <Widget /> component with a title', () => {
-    const wrapper = mount(
-      <Widget
-        title={'test widget'}
-      >
-      </Widget>
-    );
-		expect(wrapper.find('h2').length).toBe(1);
-		expect(wrapper.find('h2').text()).toBe('test widget');
-	});
-})
+  it('renders one empty <Widget /> component with a title', () => {
+    const wrapper = mount(<Widget title={'test widget'} />);
+    expect(wrapper.find('h2').length).toBe(1);
+    expect(wrapper.find('h2').text()).toBe('test widget');
+  });
+});
