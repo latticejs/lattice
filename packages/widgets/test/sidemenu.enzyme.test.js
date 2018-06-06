79d72ba0181db9c64561416c38c1ff9580beb342
Diego <paez.di@gmail.com>
2018-06-06 15:27:11 -0300

    Dk/enzyme sunburst (#66)
    * Update: add enzyme deps + config jest
    
    * Add: enzyme setup adapter
    
    * Add: sunburst enzyme test
    
    * Update: lint fix
    
    * Update: yarn lock

--- packages/widgets/test/sidemenu.enzyme.test.js
+++ packages/widgets/test/sidemenu.enzyme.test.js
@@ -8,23 +8,16 @@ const navigation = [
   {
     path: '/employees',
     title: 'Employees',
-    component: () => (<div>dummy</div>)
+    component: () => <div>dummy</div>
   }
-]
+];
 
 describe('<SideMenu />', () => {
   it('has a functioning click handler', () => {
-    const wrapper = mount(
-      <SideMenu
-        width={200}
-        navigation={navigation}
-        onItemClick={sideMenuMockClick}
-      />
-    );
-		expect(wrapper.find(SideMenu).length).toBe(1);
+    const wrapper = mount(<SideMenu width={200} navigation={navigation} onItemClick={sideMenuMockClick} />);
+    expect(wrapper.find(SideMenu).length).toBe(1);
     // simulate click on item, finding children using displayname
     wrapper.find('ButtonBase').simulate('click');
     expect(sideMenuMockClick.mock.calls.length).toEqual(1);
-	});
-})
-
+  });
+});
