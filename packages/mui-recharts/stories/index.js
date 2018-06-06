50143a03e08cf7f2045c456e7733d18d3b80c42c
Max Fierro <elmasse@gmail.com>
2018-06-06 15:49:54 -0300

    Improved mui-rechart stories

--- packages/mui-recharts/stories/index.js
+++ packages/mui-recharts/stories/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
-import { LineChart as RechartLineChart, Line } from 'recharts';
+// recharts
+import {
+  LineChart as RechartLineChart,
+  AreaChart as RechartAreaChart,
+  Area,
+  Line,
+  Tooltip,
+  ResponsiveContainer,
+  XAxis,
+  YAxis
+} from 'recharts';
+// Ours
+import muiTheme from '../.storybook/decorator-material-ui';
 import withMuiStyle from '../src';
 
+// Wrapped Charts
 const LineChart = withMuiStyle(RechartLineChart);
+const AreaChart = withMuiStyle(RechartAreaChart);
+
+// Decorators
+const AddResponsive = story => <ResponsiveContainer width="100%">{story()}</ResponsiveContainer>;
+const FullViewport = story => <div style={{ height: '100vh', width: '100vw' }}>{story()}</div>;
+
+export default ({ storiesOf, action }) => {
+  storiesOf('mui-recharts: light theme', module)
+    .addDecorator(AddResponsive)
+    .addDecorator(muiTheme())
+    .addDecorator(FullViewport)
+    .add('Area Chart: basic', () => <AreaChartBasic />)
+    .add('Area Chart: axis', () => <AreaChartAxis />)
+    .add('Line Chart: basic', () => <LineChartBasic />)
+    .add('Line Chart: axis', () => <LineChartAxis />);
+  storiesOf('mui-recharts: dark theme', module)
+    .addDecorator(AddResponsive)
+    .addDecorator(muiTheme({ palette: { type: 'dark' } }))
+    .addDecorator(FullViewport)
+    .add('Area Chart: basic', () => <AreaChartBasic />)
+    .add('Area Chart: axis', () => <AreaChartAxis />)
+    .add('Line Chart: basic', () => <LineChartBasic />)
+    .add('Line Chart: axis', () => <LineChartAxis />);
+};
 
 const data = [
   { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
@@ -14,11 +51,40 @@ const data = [
   { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }
 ];
 
-export default ({ storiesOf, action }) => {
-  storiesOf('MUI-recharts', module).add('Wrapper around LineChart', () => (
-    <LineChart width={400} height={400} data={data}>
-      <Line variant="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-      <Line variant="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-  ));
-};
+// Components
+
+const AreaChartBasic = props => (
+  <AreaChart data={data} {...props}>
+    <Area variant="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+    <Area variant="monotone" dataKey="uv" stroke="#82ca9d" />
+    <Tooltip />
+  </AreaChart>
+);
+
+const AreaChartAxis = props => (
+  <AreaChart data={data} {...props}>
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Area variant="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+    <Area variant="monotone" dataKey="uv" stroke="#82ca9d" />
+    <Tooltip />
+  </AreaChart>
+);
+
+const LineChartBasic = props => (
+  <LineChart data={data} {...props}>
+    <Line variant="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+    <Line variant="monotone" dataKey="uv" stroke="#82ca9d" />
+    <Tooltip />
+  </LineChart>
+);
+
+const LineChartAxis = props => (
+  <LineChart data={data} {...props}>
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Line variant="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+    <Line variant="monotone" dataKey="uv" stroke="#82ca9d" />
+    <Tooltip />
+  </LineChart>
+);
