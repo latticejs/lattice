71de68788251fee4f052f6aca3141081607ccf9d
Max Fierro <elmasse@gmail.com>
2018-06-07 20:08:25 -0300

    remove console.log

--- packages/mui-recharts/.storybook/config.js
+++ packages/mui-recharts/.storybook/config.js
@@ -3,7 +3,6 @@ import * as storybook from '@storybook/react';
 const req = require.context('../stories', true, /[^\/]+.js$/);
 function loadStories() {
   req.keys().forEach(path => {
-    console.log(path)
     const story = req(path).default;
     story(storybook);
   });
