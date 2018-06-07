3ade119526f47bdaa43a9c5e3b6721b32403ea25
Diego <paez.di@gmail.com>
2018-06-07 10:44:12 -0300

    Dk/enzyme mui recharts (#63)
    * Update: add enzyme related deps and jest config
    
    * Add: enzyme setup
    
    * Add: new mui-recharts enzyme test
    
    * added lint/formatter: CRA + prettier (#59)
    
    * added lint/formatter: CRA + prettier
    
    * updated snapshot
    
    * fixed lint errors and deprecated react lifecycle
    
    * Dk/enzyme sunburst (#66)
    
    * Update: add enzyme deps + config jest
    
    * Add: enzyme setup adapter
    
    * Add: sunburst enzyme test
    
    * Update: lint fix
    
    * Update: yarn lock
    
    * Update: add enzyme related deps and jest config
    
    * Add: enzyme setup
    
    * Add: new mui-recharts enzyme test
    
    * Update: linted :no_entry_sign: :ghost:

---
+++ packages/mui-recharts/test/muirecharts.enzyme.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import withMuiStyle from '../';
+
+class Dummy extends React.Component {
+  static displayName = 'DummyComponent';
+  render() {
+    return <div className={this.props.classes.root} />;
+  }
+}
+
+describe('<MuiRecharts />', () => {
+  it('renders one <Dummy /> component wrapped up with mui recharts', () => {
+    const Wrapped = withMuiStyle(Dummy);
+    const wrapper = shallow(<Wrapped />);
+
+    expect(wrapper.find('MuiRechart(DummyComponent)').length).toBe(1);
+    expect(
+      wrapper
+        .find('MuiRechart(DummyComponent)')
+        .dive()
+        .props().theme
+    ).toBeDefined();
+  });
+});
