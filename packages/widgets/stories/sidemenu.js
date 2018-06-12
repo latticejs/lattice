5849be7e6b14967cd9a861d1bea0cac41c4d46e7
Max Fierro <elmasse@gmail.com>
2018-06-12 15:58:43 -0300

    Improved widgets storybook (#77)
    * Improved widgets storybook
    
    * Fix issue with Grid and paddings in stories.

---
+++ packages/widgets/stories/sidemenu.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+// Material UI
+import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+// Material UI Icons
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
+import MapIcon from '@material-ui/icons/Map';
+import PageViewIcon from '@material-ui/icons/Pageview';
+import PeopleIcon from '@material-ui/icons/People';
+
+// Ours
+import { SideMenu } from '../src';
+import muiTheme from '../.storybook/decorator-material-ui';
+
+// Decorators
+
+const InGrid = story => (
+  <Grid container spacing={24}>
+    <Drawer open variant="persistent">
+      {story()}
+    </Drawer>
+  </Grid>
+);
+
+const Flexed = story => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>{story()}</div>
+);
+const FullViewport = story => <div style={{ height: '100vh', width: '100vw', padding: 12 }}>{story()}</div>;
+
+export default ({ storiesOf, action }) => {
+  storiesOf('widgets/SideMenu', module)
+    .addDecorator(InGrid)
+    .addDecorator(Flexed)
+    .addDecorator(muiTheme())
+    .addDecorator(FullViewport)
+    .add('basic', () => (
+      <Styled>
+        {classes => <SideMenu navigation={navigation} className={classes.flexed} onItemClick={action('click')} />}
+      </Styled>
+    ))
+    .add('selected', () => (
+      <Styled>
+        {classes => (
+          <SideMenu
+            navigation={navigation}
+            activeRoute={navigation[0]}
+            className={classes.flexed}
+            onItemClick={action('click')}
+          />
+        )}
+      </Styled>
+    ))
+    .add('mini selected', () => (
+      <Styled>
+        {classes => (
+          <SideMenu
+            mini
+            navigation={navigation}
+            activeRoute={navigation[0]}
+            className={classes.flexed}
+            onItemClick={action('click')}
+          />
+        )}
+      </Styled>
+    ));
+};
+
+const styles = theme => ({
+  flexed: {
+    flex: 1
+  }
+});
+
+const Styled = withStyles(styles)(({ classes, children }) => {
+  return children(classes);
+});
+
+const navigation = [
+  {
+    path: '/dashboard',
+    title: 'Dashboard',
+    icon: DashboardIcon
+  },
+  {
+    path: '/employees',
+    title: 'Employees',
+    icon: PeopleIcon
+  },
+  {
+    title: 'Theme',
+    icon: InsertChartIcon,
+    children: [
+      {
+        path: '/map',
+        title: 'Maps',
+        icon: MapIcon
+      },
+      {
+        path: '/pageview',
+        title: 'Page Views',
+        icon: PageViewIcon
+      }
+    ]
+  }
+];
---
+++ packages/widgets/stories/widget.js
@@ -0,0 +1,122 @@
+import React from 'react';
+
+// Material UI
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+// Ours
+import { Widget } from '../src';
+import muiTheme from '../.storybook/decorator-material-ui';
+
+// Decorators
+
+const InGrid = story => (
+  <Grid container spacing={24}>
+    <Grid item xs={12}>
+      {story()}
+    </Grid>
+  </Grid>
+);
+
+const Flexed = story => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>{story()}</div>
+);
+const FullViewport = story => <div style={{ height: '100vh', width: '100vw', padding: 12 }}>{story()}</div>;
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. A pellentesque sit amet porttitor eget. Aenean sed adipiscing diam donec adipiscing tristique risus.`;
+
+export default ({ storiesOf, action }) => {
+  storiesOf('widgets/Widget', module)
+    .addDecorator(InGrid)
+    .addDecorator(Flexed)
+    .addDecorator(muiTheme())
+    .addDecorator(FullViewport)
+    .add('basic', () => <Widget title="Title">{lorem}</Widget>)
+    .add('featured', () => (
+      <Widget featured title="Title">
+        {lorem}
+      </Widget>
+    ))
+    .add('with borders', ({ classes }) => (
+      <Styled>
+        {classes => (
+          <Grid container spacing={24}>
+            <Grid item xs={6}>
+              <Widget title="Border top" border="top">
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border bottom" border="bottom">
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border top custom" border="top" classes={{ border: classes.customTopBorder }}>
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border bottom custom" border="bottom" classes={{ border: classes.customBotomBorder }}>
+                {lorem}
+              </Widget>
+            </Grid>
+          </Grid>
+        )}
+      </Styled>
+    ));
+
+  storiesOf('widgets/Widget (dark theme)', module)
+    .addDecorator(InGrid)
+    .addDecorator(Flexed)
+    .addDecorator(muiTheme({ palette: { type: 'dark' } }))
+    .addDecorator(FullViewport)
+    .add('basic', () => <Widget title="Title">{lorem}</Widget>)
+    .add('featured', () => (
+      <Widget featured title="Title">
+        {lorem}
+      </Widget>
+    ))
+    .add('with borders', ({ classes }) => (
+      <Styled>
+        {classes => (
+          <Grid container spacing={24}>
+            <Grid item xs={6}>
+              <Widget title="Border top" border="top">
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border bottom" border="bottom">
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border top custom" border="top" classes={{ border: classes.customTopBorder }}>
+                {lorem}
+              </Widget>
+            </Grid>
+            <Grid item xs={6}>
+              <Widget title="Border bottom custom" border="bottom" classes={{ border: classes.customBotomBorder }}>
+                {lorem}
+              </Widget>
+            </Grid>
+          </Grid>
+        )}
+      </Styled>
+    ));
+};
+
+const styles = theme => ({
+  customTopBorder: {
+    borderColor: 'red'
+  },
+  customBotomBorder: {
+    borderWidth: 2,
+    borderColor: 'green'
+  }
+});
+
+const Styled = withStyles(styles)(({ classes, children }) => {
+  return children(classes);
+});
