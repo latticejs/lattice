b219e2434dfa49fb1917739762e4661cf7c862a6
Diego Paez <paez.di@gmail.com>
2018-06-13 15:34:37 -0300

    Update: add support for onEdgeAdded cb

--- packages/dag/src/index.js
+++ packages/dag/src/index.js
@@ -36,8 +36,17 @@ const styles = theme => ({
 
 class Dag extends Component {
   static displayName = 'Dag';
+  static defaultProps = {
+    editable: false
+  };
 
-  state = {};
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      newEdge: {},
+      initNode: true
+    };
+  }
 
   componentDidMount() {
     this.dagcore = new DagCore(this.root, { ...this.props, ...this.state });
@@ -48,15 +57,56 @@ class Dag extends Component {
     this.dagcore.destroyGraph();
   }
 
+  resetSelection() {
+    return {
+      newEdge: {},
+      initNode: true
+    };
+  }
+
+  editSelectedNode = node => {
+    let { newEdge } = this.state;
+    const newEdgeKey = this.state.initNode ? 'source' : 'target';
+    // avoid self-directed nodes
+    if (node === newEdge.source) {
+      this.setState(this.resetSelection());
+      return;
+    }
+
+    newEdge[newEdgeKey] = node;
+    if (newEdgeKey === 'target') {
+      // trigger new edge cb
+      this.props.onEdgeAdded(newEdge);
+      // reset state
+      newEdge = {};
+    }
+
+    this.setState({
+      initNode: !this.state.initNode,
+      newEdge
+    });
+  };
+
   render() {
-    const { width, height, classes = {} } = this.props;
+    const { width, height, classes = {}, editable, onClickNode, onClickEdge } = this.props;
     const rootClasses = [classes.root];
 
     const nodes = this.props.nodes.map((node, i) => {
-      return <Node data={node} name={node.title} key={`node-${i}`} classes={classes} />;
+      return (
+        <Node
+          data={node}
+          name={node.title}
+          key={`node-${i}`}
+          classes={classes}
+          editSelectedNode={this.editSelectedNode}
+          editable={editable}
+          onClickNode={onClickNode}
+        />
+      );
     });
+
     const edges = this.props.edges.map((edge, i) => {
-      return <Edge key={edge.target + i} data={edge} classes={classes} />;
+      return <Edge key={edge.target + i} data={edge} classes={classes} editable={editable} onClickEdge={onClickEdge} />;
     });
 
     return (
