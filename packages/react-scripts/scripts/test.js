952315c644712acfa242ac70e5da6e0d53bc5829
Max Fierro <elmasse@gmail.com>
2018-06-18 14:18:51 -0300

    remove --watch arg append from react-scripts

--- packages/react-scripts/scripts/test.js
+++ packages/react-scripts/scripts/test.js
@@ -25,11 +25,6 @@ require('../config/env');
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
-}
-
 // @remove-on-eject-begin
 // This is not necessary after eject because we embed config into package.json.
 const createJestConfig = require('./utils/createJestConfig');
