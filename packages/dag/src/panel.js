20deb36b854bfa93d082f538bc476a9f851f8759
Diego Paez <paez.di@gmail.com>
2018-07-01 19:06:38 -0300

    Update: improvements around panel layout and theme usage

--- packages/dag/src/panel.js
+++ packages/dag/src/panel.js
@@ -4,21 +4,20 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   card: {
-    display: 'flex',
     position: 'absolute'
   },
   details: {
-    display: 'flex',
-    flexDirection: 'column'
+    backgroundColor: theme.palette.primary[theme.palette.type],
+    color: theme.palette.background.default
   },
   content: {
     flex: '1 0 auto'
   },
   controls: {
-    display: 'flex',
     alignItems: 'center',
     paddingLeft: theme.spacing.unit,
     paddingBottom: theme.spacing.unit
@@ -44,40 +43,55 @@ class GraphPanel extends Component {
 
   renderContentNode({ title }) {
     return (
-      <React.Fragment>
-        <Typography variant="body1">Node:</Typography>
-        <Typography variant="body1" color="textSecondary">
+      <Grid item xs={12}>
+        <Typography variant="body1" color="default">
+          Node:
+        </Typography>
+        <Typography variant="body1" color="inherit">
           {title}
         </Typography>
-      </React.Fragment>
+      </Grid>
     );
   }
 
   renderContentEdge({ source, target }) {
     return (
-      <React.Fragment>
-        <div>
-          <Typography variant="caption">Source</Typography>
-          <Typography variant="body1">{source}</Typography>
-        </div>
-        <div>
-          <Typography variant="caption">Target</Typography>
-          <Typography variant="body1">{target}</Typography>
-        </div>
-      </React.Fragment>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Typography variant="caption" color="default">
+            Source
+          </Typography>
+          <Typography variant="body2" color="inherit">
+            {source}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="caption" color="default">
+            Target
+          </Typography>
+          <Typography variant="body2" color="inherit">
+            {target}
+          </Typography>
+        </Grid>
+      </Grid>
     );
   }
 
   render() {
-    const { children, classes, title, source, target, style } = this.props;
+    const { children, classes, title, source, target, style, actions = {} } = this.props;
+
     return createPortal(
       <Card className={classes.panel} style={style}>
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            {title ? this.renderContentNode({ title }) : this.renderContentEdge({ source, target })}
-          </CardContent>
-        </div>
-        <div className={classes.controls}>{children && children()}</div>
+        <Grid item container alignItems="center">
+          <Grid item xs={12} className={classes.details}>
+            <CardContent className={classes.content}>
+              {title ? this.renderContentNode({ title }) : this.renderContentEdge({ source, target })}
+            </CardContent>
+          </Grid>
+          <Grid item xs={12}>
+            <div className={classes.controls}>{children && children({ ...actions })}</div>
+          </Grid>
+        </Grid>
       </Card>,
       this.el
     );
