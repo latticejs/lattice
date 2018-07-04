dae3e65f20cbb21506071a42b11ad36f58cebba9
Diego Paez <paez.di@gmail.com>
2018-07-04 12:26:41 -0300

    Update: interaction test now supports panel interaction

--- packages/dag/test/enzyme.test.js
+++ packages/dag/test/enzyme.test.js
@@ -20,11 +20,18 @@ describe('<Dag />', () => {
     expect(wrapper.find('.dag-wrapper').length).toBe(1);
     expect(onEdgeAdded).not.toHaveBeenCalled();
     wrapper
-      .find(`.${DAG_DEFAULTS.nodeClass}`)
+      .find(`.${DAG_DEFAULTS.nodeClass}`) // click on a node, open panel
       .at(0)
       .simulate('click');
     wrapper
-      .find(`.${DAG_DEFAULTS.nodeClass}`)
+      .find(`.${DAG_DEFAULTS.nodeClass}`) // click on a node, open panel
+      .at(0)
+      .find('button')
+      .at(1)
+      .find('SvgIcon')
+      .simulate('click');
+    wrapper
+      .find(`.${DAG_DEFAULTS.nodeClass}`) // click on another node
       .at(1)
       .simulate('click');
     expect(onEdgeAdded).toHaveBeenCalled();
