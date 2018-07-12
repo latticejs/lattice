ac8c121d1d597dd564b0251c4f4b81964f8eab0b
Diego Paez <paez.di@gmail.com>
2018-07-12 11:57:05 -0300

    Update: moved files

--- packages/clap/lib/installDeps.js
+++
@@ -1,7 +0,0 @@
-var exec = require('execa');
-
-module.exports = function installDeps(projectPath) {
-  var command = 'npm install';
-  process.chdir(projectPath);
-  return exec.shell(command);
-};
