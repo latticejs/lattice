9d0400d4ca868369f9418f16c452cf13310f6d2c
Diego Paez <paez.di@gmail.com>
2018-07-16 15:43:56 -0300

    Update: add assert msg

--- packages/clap/lib/tasks/downloadExample.js
+++ packages/clap/lib/tasks/downloadExample.js
@@ -4,8 +4,8 @@ var exec = require('execa');
 module.exports = async function downloadExample(project, example) {
   assert.ok(project);
   assert.ok(example);
-  assert.strictEqual(typeof project, 'string');
-  assert.strictEqual(typeof example, 'string');
+  assert.strictEqual(typeof project, 'string', 'clap: project should be a string');
+  assert.strictEqual(typeof example, 'string', 'clap: example should be a string');
   // NOTE(dk): we should split these commands (curl, tar) to have better errors.
   var command = `curl --fail --silent --show-error https://codeload.github.com/latticejs/lattice/tar.gz/master | tar -xz -C ${project} --strip=3 lattice-master/examples/${example}`;
   var out = {};
