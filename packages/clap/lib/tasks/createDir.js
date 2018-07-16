a39e37c8c316f767d95d2b377484bdfcae39a22f
Diego Paez <paez.di@gmail.com>
2018-07-16 15:42:15 -0300

    Update: add assert msg

--- packages/clap/lib/tasks/createDir.js
+++ packages/clap/lib/tasks/createDir.js
@@ -3,7 +3,7 @@ var exec = require('execa');
 
 module.exports = function createDir(projectName) {
   assert.ok(projectName);
-  assert.strictEqual(typeof projectName, 'string');
+  assert.strictEqual(typeof projectName, 'string', 'clap: createDir expects a string');
   var command = `mkdir -p ${projectName}`;
   return exec.shell(command);
 };
