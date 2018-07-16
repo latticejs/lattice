c2c8dccafd5bf1b062d1223c1ddb96724ecd37d9
Diego Paez <paez.di@gmail.com>
2018-07-16 15:50:23 -0300

    Add: new list task

---
+++ packages/clap/lib/tasks/listExamples.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var octokit = require('@octokit/rest')();
+
+module.exports = function listExamples(options) {
+  assert.equal(typeof options, 'object', 'clap: listExamples expects an object');
+
+  return octokit.repos.getContent({
+    owner: options.owner,
+    repo: options.repo,
+    path: options.path,
+    ref: options.ref
+  });
+};
