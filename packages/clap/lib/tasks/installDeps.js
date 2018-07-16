f992b6e9078f4282eb0914ff918c1edaf14405fc
Diego Paez <paez.di@gmail.com>
2018-07-16 15:44:44 -0300

    Update: add assert msg

--- packages/clap/lib/tasks/installDeps.js
+++ packages/clap/lib/tasks/installDeps.js
@@ -3,7 +3,7 @@ var exec = require('execa');
 
 module.exports = function installDeps(projectPath) {
   assert.ok(projectPath);
-  assert.strictEqual(typeof projectPath, 'string');
+  assert.strictEqual(typeof projectPath, 'string', 'clap: installDeps expects a string');
   var command = 'npm install';
   process.chdir(projectPath);
   return exec.shell(command);
