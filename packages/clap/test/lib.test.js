fc48de07be7e94e044c1d4989f617369a8a19c3c
Diego Paez <paez.di@gmail.com>
2018-07-17 16:22:00 -0300

    Update: test (initial version)

--- packages/clap/test/lib.test.js
+++ packages/clap/test/lib.test.js
@@ -1,25 +1,29 @@
 // \\ CLAP test suite \\
 // manual mock first
 jest.mock('../lib/tasks', () => ({
-  createDir: jest.fn(() => Promise.resolve({ code: 1 })),
+  createDir: jest.fn(() => Promise.resolve({ code: 0 })),
   download: jest.fn(() => Promise.resolve({ code: 0 })),
   install: jest.fn(() => Promise.resolve({ code: 0 }))
 }));
 
+var process = require('process');
 var lib = require('../lib');
 var DEFAULT_EXAMPLE = require('../cli').DEFAULT_EXAMPLE;
 var DEFAULT_PROJECT = 'demo';
 
 // Describe: API
 describe('clap', () => {
-  test('call clap with a projectName should return exit(0)', async () => {
-    /* WIP - test kind of work but they leave "open handles" so jest doesnt exit properly
-    expect.assertions(1);
-    var exitSpy = jest.spyOn(process, 'exit').mockImplementation(jest.fn());
+  test('call clap with a projectName should return exit(0)', async done => {
+    /* WIP - test kind of work but they leave "open handles" so jest doesnt exit properly */
 
-    await lib.clap(DEFAULT_EXAMPLE, DEFAULT_PROJECT);
-    expect(exitSpy).toHaveBeenCalledWith(1);
-    exitSpy.mockRestore();
-    */
+    jest.spyOn(process, 'exit').mockImplementation(code => {
+      if (code === 0) return 'ok';
+      if (code === 1) throw new Error('process.exit() was called.');
+    });
+
+    // useful for testing the bad scenario
+    // await expect(lib.clap(DEFAULT_EXAMPLE, DEFAULT_PROJECT)).rejects.toThrow('process.exit() was called.');
+    await expect(lib.clap(DEFAULT_EXAMPLE, DEFAULT_PROJECT)).resolves.not.toThrow();
+    done();
   });
 });
