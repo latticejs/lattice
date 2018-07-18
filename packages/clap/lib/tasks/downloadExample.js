c19c113798a5a95ab63c15fa9d0fe432e7867828
Diego Paez <paez.di@gmail.com>
2018-07-18 16:49:45 -0300

    Update: refactor download to use got instead of curl

--- packages/clap/lib/tasks/downloadExample.js
+++ packages/clap/lib/tasks/downloadExample.js
@@ -1,5 +1,8 @@
 var assert = require('assert');
-var exec = require('execa');
+var got = require('got');
+var tar = require('tar');
+
+var BASE_URL = 'https://codeload.github.com/latticejs/lattice/tar.gz/master';
 
 module.exports = async function downloadExample(project, example) {
   assert.ok(project);
@@ -7,12 +10,35 @@ module.exports = async function downloadExample(project, example) {
   assert.strictEqual(typeof project, 'string', 'clap: project should be a string');
   assert.strictEqual(typeof example, 'string', 'clap: example should be a string');
   // NOTE(dk): we should split these commands (curl, tar) to have better errors.
-  var command = `curl --fail --silent --show-error https://codeload.github.com/latticejs/lattice/tar.gz/master | tar -xz -C ${project} --strip=3 lattice-master/examples/${example}`;
-  var out = {};
-  try {
-    out = await exec.shell(command);
-  } catch (err) {
-    out = err;
-  }
-  return out;
+
+  return new Promise(function(resolve, reject) {
+    got
+      .stream(BASE_URL)
+      .on('error', function(err) {
+        reject({
+          code: 1,
+          stderr: err
+        });
+      })
+      .pipe(
+        tar.extract(
+          {
+            // Extract to the project name
+            cwd: project,
+            // Strip the first 3 dirs
+            strip: 3
+          },
+          [`lattice-master/examples/${example}`]
+        )
+      )
+      .on('error', function(err) {
+        reject({
+          code: 1,
+          stderr: err
+        });
+      })
+      .on('end', function() {
+        resolve({ code: 0 });
+      });
+  });
 };
