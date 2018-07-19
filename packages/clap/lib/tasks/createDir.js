b51cbc34a1c532e8ce24ab0986b4a27458e36b84
Diego Paez <paez.di@gmail.com>
2018-07-19 10:44:12 -0300

    Update: switch from linux mkdir to a lib wrapper for _better_ OS interop

--- packages/clap/lib/tasks/createDir.js
+++ packages/clap/lib/tasks/createDir.js
@@ -1,9 +1,19 @@
-var assert = require('assert');
-var exec = require('execa');
+const assert = require('assert');
+const makeDir = require('make-dir');
 
 module.exports = function createDir(projectName) {
   assert.ok(projectName);
   assert.strictEqual(typeof projectName, 'string', 'clap: createDir expects a string');
-  var command = `mkdir -p ${projectName}`;
-  return exec.shell(command);
+  return new Promise(function(resolve, reject) {
+    makeDir(projectName)
+      .then(function() {
+        resolve({ code: 0 });
+      })
+      .catch(function(err) {
+        reject({
+          code: 1,
+          stderr: err
+        });
+      });
+  });
 };
