329b8b069b15805008ba764e8ea6d33e744935d2
Diego Paez <paez.di@gmail.com>
2018-08-07 16:41:29 -0300

    Update: more enzyme tests :cool:

--- packages/tree/test/tree.enzyme.test.js
+++ packages/tree/test/tree.enzyme.test.js
@@ -21,10 +21,48 @@ const sampleInput = [
 ];
 
 describe('<Tree />', () => {
-  it('renders <Tree />', () => {
+  it('renders a <Tree />', () => {
     const wrapper = mount(<Tree treeData={sampleInput} />);
-    const targetTree = '.tree-wrapper';
-    expect(wrapper.find(targetTree).length).toBe(3);
+    expect(wrapper.find('li').length).toBe(2);
+    expect(wrapper.find(Tree).length).toBe(1);
   });
-  it('renders <Tree /> with 4 childrens (2 root lvl and 2 lvl-1)', () => {});
+  it('renders a <Tree expandedAll /> with 4 childrens (2 root lvl and 2 lvl-1)', () => {
+    const wrapper = mount(<Tree treeData={sampleInput} expandedAll />);
+    expect(wrapper.find('li').length).toBe(4);
+  });
+  it('renders a <Tree onCheckItem={cb}/> with onCheckItem cb defined and triggered', () => {
+    const onCheckItemCb = jest.fn();
+    const item = { items: [sampleInput[0]], check: true };
+    const wrapper = mount(<Tree treeData={sampleInput} onCheckItem={onCheckItemCb} />);
+    wrapper
+      .find('li')
+      .at(0)
+      .find('input[type="checkbox"]')
+      .simulate('change', { target: { checked: true } });
+    expect(onCheckItemCb).toBeCalledWith(item);
+  });
+  it('renders a <Tree onUnfoldItem={cb}/> with onUnfoldItem cb defined and triggered', () => {
+    const onUnfoldItemCb = jest.fn();
+    const item = sampleInput[1];
+    const wrapper = mount(<Tree treeData={sampleInput} onUnfoldItem={onUnfoldItemCb} />);
+    wrapper
+      .find('li')
+      .at(1)
+      .find('div')
+      .first()
+      .simulate('click');
+    expect(onUnfoldItemCb).toBeCalledWith(item);
+  });
+  it('renders a <Tree onFoldItem={cb}/> with onFoldItem cb defined and triggered', () => {
+    const onFoldItemCb = jest.fn();
+    const item = sampleInput[1];
+    const wrapper = mount(<Tree treeData={sampleInput} expandedAll onFoldItem={onFoldItemCb} />);
+    wrapper
+      .find('li')
+      .at(1)
+      .find('div')
+      .first()
+      .simulate('click');
+    expect(onFoldItemCb).toBeCalledWith(item);
+  });
 });
