be49e286ffdfe88c5d340e67b786c9dfbc0e24ed
Esteban <esteban.primost@gmail.com>
2018-09-20 10:14:56 -0300

    Storybook: Build docs public version (#210)
    * Set parser (warning msgs)
    
    * Storybook public docs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,8 +2,13 @@ module.exports = config => {
   config.module.rules.push({
     test: /\.jsx?$/,
     include: /packages\/((?!node_modules).)*\/stories\/[^\/]+\.js$/,
-    loader: require.resolve("@storybook/addon-storysource/loader"),
-    enforce: "pre"
+    loader: require.resolve('@storybook/addon-storysource/loader'),
+    options: {
+      prettierConfig: {
+        parser: 'babylon'
+      }
+    },
+    enforce: 'pre'
   });
 
   return config;
