86574c772eca53dc984b8a1f4d4d3cd863c98108
Jitesh gupta <jiteshgupta1995@gmail.com>
2019-01-10 15:50:12 +0530

    Update: default material ui for gauge added

--- packages/gauge/src/index.js
+++ packages/gauge/src/index.js
@@ -26,4 +26,35 @@ class RadialGaugeComponent extends React.Component {
   }
 }
 
+RadialGaugeComponent.defaultProps = {
+  fontValue: 'Roboto, sans-serif',
+  fontNumbers: 'Roboto, sans-serif',
+  fontUnits: 'Roboto, sans-serif',
+  value: 30,
+  units: 'mph',
+  width: 300,
+  height: 300,
+  barWidth: '5',
+  barShadow: '0',
+  colorBarProgress: '#3f51b5',
+  colorBar: '#c5cbe9',
+  borderShadowWidth: '20',
+  borderInnerWidth: '0',
+  borderOuterWidth: '0',
+  borderMiddleWidth: '0',
+  highlights: 'false',
+  valueBoxStroke: '0',
+  needleWidth: '3',
+  animateOnInit: 'true',
+  animatedValue: 'true',
+  animationDuration: '1500',
+  animationRule: 'linear',
+  colorValueBoxShadow: '0',
+  valueBoxBorderRadius: '0',
+  valueTextShadow: '0',
+  colorNeedle: '#e91f62',
+  needleType: 'arrow',
+  colorValueBoxBackground: 'transparent'
+};
+
 export default RadialGaugeComponent;
