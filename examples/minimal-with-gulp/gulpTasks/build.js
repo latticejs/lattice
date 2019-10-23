eef268ace9d47195dc4b3175a4884811fc1cb260
Suresh <suresh.chaurasia@celestialsys.com>
2019-10-23 12:10:20 +0530

    Added gulp task

--- examples/minimal-with-gulp/gulpTasks/build.js
+++ examples/minimal-with-gulp/gulpTasks/build.js
@@ -3,22 +3,22 @@ import webpackConfig from '../webpack.config';
 import webpackStream from 'webpack-stream';
 
 gulp.task('build:dev', () => {
-  process.env.NODE_ENV = JSON.stringify('dev');
+  process.env.NODE_ENV = JSON.stringify('development');
   process.env.REACT_SPINKIT_NO_STYLES = JSON.stringify(false);
 
   return gulp
     .src('src/index.js')
-    .pipe(webpackStream(webpackConfig))
+    .pipe(webpackStream(webpackConfig.dev))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:prod', () => {
-  process.env.NODE_ENV = JSON.stringify('prod');
+  process.env.NODE_ENV = JSON.stringify('production');
   process.env.REACT_SPINKIT_NO_STYLES = JSON.stringify(false);
 
   return gulp
     .src('src/index.js')
-    .pipe(webpackStream(webpackConfig))
+    .pipe(webpackStream(webpackConfig.prod))
     .pipe(gulp.dest('build'));
 });
 
--- examples/minimal-with-gulp/gulpTasks/startServer.js
+++ examples/minimal-with-gulp/gulpTasks/startServer.js
@@ -11,12 +11,16 @@ import webpackMiddleware from 'webpack-dev-middleware';
 
 gulp.task('startServer:dev', () => {
   const app = express();
-  const port = 4500;
+  const port = 3000;
   const baseDir = 'dist';
-  const compiler = webpack(webpackConfig);
+  const compiler = webpack(webpackConfig.dev);
+
   app.use(
     webpackMiddleware(compiler, {
-      noInfo: true
+      noInfo: true,
+      hot: true,
+      inline: true,
+      publicPath: webpackConfig.dev.output.publicPath
     })
   );
   app.use(webpackHotMiddleware(compiler));
@@ -30,3 +34,39 @@ gulp.task('startServer:dev', () => {
     console.log(`Listening on ${port}...`);
   });
 });
+
+gulp.task('startServer:prod', () => {
+  const app = express();
+  const port = 3000;
+  const baseDir = 'build';
+
+  app.use(helmet());
+  app.get('*.js', (req, res) => {
+    if (req.url.indexOf('bundle') > -1) {
+      res.set('Content-Type', 'application/octet-stream; charset=UTF-8');
+      res.set('Content-Encoding', 'gzip');
+      res.set('X-Content-Type-Options', 'no');
+      res.sendFile(path.join(rootDir.get(), baseDir, '/bundle.js.gz'));
+    }
+  });
+
+  app.get('*.css', (req, res) => {
+    res.set('Content-Encoding', 'gzip');
+    res.type('text/css; charset=UTF-8');
+    res.removeHeader('Content-Length');
+    res.removeHeader('Cache-Control');
+    res.set('Vary', 'Accept-Encoding');
+    res.sendFile(path.join(rootDir.get(), baseDir, '/bundle.css.gz'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(rootDir.get(), baseDir, '/src/index.html'));
+  });
+
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Listening on ${port}...`);
+  });
+});
+
+gulp.task('startServer', () => gulp.series('startServer:prod', 'startServer:dev'));
