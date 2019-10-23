6674abd5b6e27b9effe4b3faee195b69eb0344ba
Palak Rathi <prathi@celestialsys.com>
2019-10-23 16:56:33 +0530

    Updated Main.test.js file.

--- examples/minimal/tests/Main.test.js
+++ examples/minimal/tests/Main.test.js
@@ -1,18 +1,21 @@
 import { configure } from 'enzyme';
-import { createShallow } from '@material-ui/core/test-utils';
+import { createMount } from '@material-ui/core/test-utils';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../src/App.js';
+import Main from '../src/Main.js';
 import React from 'react';
 
 describe('Test Main component', () => {
   configure({ adapter: new Adapter() });
-  let shallow;
+  let mount;
+  let childWrapper;
 
   beforeAll(() => {
-    shallow = createShallow();
+    mount = createMount({ untilSelector: 'Main' });
+    childWrapper = mount(<Main />);
   });
 
-  it('shallow test App component', () => {
-    shallow(<App />);
+  it('should render App component', () => {
+    expect(childWrapper.find(App)).toHaveLength(1);
   });
 });
