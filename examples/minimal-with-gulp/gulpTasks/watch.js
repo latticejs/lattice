30dc85ea3fd442ce99c303eb661972f1fc8bbf55
Suresh <suresh.chaurasia@celestialsys.com>
2019-10-26 22:48:35 +0530

    Fixed the pureRender issue

--- examples/minimal-with-gulp/gulpTasks/watch.js
+++ examples/minimal-with-gulp/gulpTasks/watch.js
@@ -2,6 +2,5 @@ import gulp from 'gulp';
 import { lint } from './lint';
 
 gulp.task('watch', () => {
-  gulp.watch(['src/*.jsx', 'src/**/*.js']);
+  gulp.watch(['./src/*.jsx', './src/*.js'], gulp.series(lint));
 });
-gulp.task('watch', gulp.series('watch', lint));
