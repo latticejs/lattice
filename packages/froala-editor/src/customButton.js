fe119d2e4d89fe96fdb2508229ce597c3ff90c6a
Palak Rathi <prathi@celestialsys.com>
2020-01-06 14:12:50 +0530

    Added custom Buttons.

--- packages/froala-editor/src/customButton.js
+++ packages/froala-editor/src/customButton.js
@@ -1,22 +1,7 @@
 import Froalaeditor from 'froala-editor';
 
-// import bold from '@material-ui/icons/WbSunnyOutlined';
-// Froalaeditor.DefineIcon('bold', {
-//   NAME: 'info',
-//   SRC: bold
-// });
-// Froalaeditor.RegisterCommand('bold', {
-//   title: 'Hello',
-//   focus: false,
-//   undo: false,
-//   refreshAfterCallback: false,
-//   callback: function() {
-//     alert('Hello!');
-//   }
-// });
-
-Froalaeditor.DefineIconTemplate('material_design', '<i class="zmdi zmdi-[NAME]"></i>');
-// Froalaeditor.ICON_DEFAULT_TEMPLATE = 'material_design';
+Froalaeditor.DefineIconTemplate('material_design', '<i class="zmdi zmdi-[NAME] design"></i>');
+// Froalaeditor.DefineIconTemplate('material_design', '<i class="material-icons">[NAME]</i>');
 Froalaeditor.DefineIcon('bold', { NAME: 'format-bold', template: 'material_design' });
 Froalaeditor.RegisterCommand('bold', {
   title: 'Bold',
@@ -25,4 +10,51 @@ Froalaeditor.RegisterCommand('bold', {
   undo: true
 });
 
+Froalaeditor.DefineIcon('italic', { NAME: 'format-italic', template: 'material_design' });
+Froalaeditor.RegisterCommand('italic', {
+  title: 'Italic',
+  icon: 'italic',
+  // focus: true,
+  undo: true
+});
+
+Froalaeditor.DefineIcon('underline', { NAME: 'format-underlined', template: 'material_design' });
+Froalaeditor.RegisterCommand('underline', {
+  title: 'Underline',
+  icon: 'underline',
+  // focus: true,
+  undo: true
+});
+
+Froalaeditor.DefineIcon('strikeThrough', { NAME: 'format-strikethrough', template: 'material_design' });
+Froalaeditor.RegisterCommand('strikeThrough', {
+  title: 'StrikeThrough',
+  icon: 'strikeThrough',
+  focus: true,
+  undo: true
+});
+
+Froalaeditor.DefineIcon('emoticons', { NAME: 'insert-emoticon', template: 'material_design' });
+Froalaeditor.RegisterCommand('emoticons', {
+  title: 'Emoticons',
+  icon: 'emoticons',
+  focus: true,
+  undo: true
+});
+
+Froalaeditor.DefineIcon('selectAll', { NAME: 'select-all', template: 'material_design' });
+Froalaeditor.RegisterCommand('selectAll', {
+  title: 'Select All',
+  icon: 'selectAll',
+  focus: true,
+  undo: true
+});
+
+Froalaeditor.DefineIcon('html', { NAME: 'code', template: 'material_design' });
+Froalaeditor.RegisterCommand('html', {
+  title: 'Code View',
+  icon: 'html',
+  focus: true,
+  undo: true
+});
 export default Froalaeditor;
