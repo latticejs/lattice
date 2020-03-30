dcb445945cfa1b6b5da6ea3c779e72649e2ba6ef
Rahulgarg30591 <rgarg@celestialsys.com>
2020-03-30 15:56:32 +0530

    getContent is now changed to getContents

--- packages/clap/lib/tasks/listExamples.js
+++ packages/clap/lib/tasks/listExamples.js
@@ -4,7 +4,7 @@ var octokit = require('@octokit/rest')();
 module.exports = function listExamples(options) {
   assert.equal(typeof options, 'object', 'clap: listExamples expects an object');
 
-  return octokit.repos.getContent({
+  return octokit.repos.getContents({
     owner: options.owner,
     repo: options.repo,
     path: options.path,
