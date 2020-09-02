3ce3047f099906cfbad26a8eb30c38ff80dcbf70
Shivani Sharma <s.sharma@celestialsys.com>
2020-09-02 12:41:36 +0530

    LJS-79 resolved prettier package issue

--- examples/apollo-enterprise/src/components/dashboard/TaskScheduler.js
+++ examples/apollo-enterprise/src/components/dashboard/TaskScheduler.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // Recharts
 import { ResponsiveContainer } from 'recharts';
 // Lattice
--- examples/apollo-enterprise/src/containers/App.js
+++ examples/apollo-enterprise/src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Material-UI
 import { createMuiTheme } from '@material-ui/core/styles';
