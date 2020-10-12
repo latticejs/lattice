04e98896afbf0daaf1d958b35e1a7a0ae196e7c0
RahulGarg <rgarg@celestialsys.com>
2020-10-12 20:18:08 +0530

    Revert "LJS-117 | Added SaaSStarter into Lattice examples"

--- examples/SaaSStarter/client/tests/dummy.test.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env jest */
-describe('Dummy Test Suite', () => {
-  it('dummy test', () => {
-    expect(true).toBe(true);
-  });
-});
--- examples/SaaSStarter/server/image-service/test/test-order.js
+++
@@ -1,9 +0,0 @@
-// This is the order of the test case file execution.
-// Execution is based on describe and it will avoid parallel execution.
-const nock = require('nock');
-
-before(async () => {
-  nock(process.env.LOG_SERVICE).persist().post('/').reply(200);
-});
-require('./upload-spec');
-require('./delete-spec');
