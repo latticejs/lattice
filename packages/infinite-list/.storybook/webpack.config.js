b9c33c206efd9ca637f7adc39a7d19e54375e9c9
Shivani Sharma <s.sharma@celestialsys.com>
2020-11-11 20:16:51 +0530

    LJS-121 added webpack config file

---
+++ packages/infinite-list/.storybook/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve('./stories'),
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { prettierConfig: { parser: 'babel' } },
+        enforce: 'pre'
+      }
+    ]
+  }
+};
